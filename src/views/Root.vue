51f5566d53eaf1520ee07f5c33d35e3c59c181e7
eduardo3g <eduardosbrasil10@gmail.com>
2021-02-05 17:22:32 -0300

    feat: add store access to UI components

--- src/views/Root.vue
+++ src/views/Root.vue
@@ -8,7 +8,33 @@
 </template>
 
 <script>
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
   name: 'Root',
+  computed: {
+    ...mapGetters('authentication', [
+      'loggedIn',
+      'user',
+    ]),
+  },
+  methods: {
+    ...mapActions('authentication', [
+      'loginUser',
+      'logoutUser',
+    ]),
+  },
+  created() {
+    onAuthUIStateChange(async (state, user) => {
+      if (state === AuthState.SignedIn) {
+        await this.loginUser(user);
+      }
+
+      if (state === AuthState.SignedOut) {
+        await this.logoutUser();
+      }
+    });
+  },
 }
 </script>
