2a7cd61a820fdda401b3dafbcd14bfd68898720a
eduardo3g <eduardosbrasil10@gmail.com>
2021-02-10 18:29:12 -0300

    style: add styles to left panel on root page

--- src/views/Root.vue
+++ src/views/Root.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    <h1>Root</h1>
+  <div class="flex w-full h-screen">
+    <!-- left side -->
+    <div class="flex w-1/2 h-full bg-blue">
+      <div class="flex items-center justify-center w-full h-full">
+        <div class="flex flex-col">
+          <div class="flex items-center">
+            <i class="fas fa-search text-white text-2xl mr-5"></i>
+            <p class="text-white font-bold text-xl">Follow your interests.</p>
+          </div>
+          <div class="flex items-center mt-10">
+            <i class="fas fa-user-friends text-white text-2xl mr-4"></i>
+            <p class="text-white font-bold text-xl">Hear what people are talking about.</p>
+          </div>
+          <div class="flex items-center mt-10">
+            <i class="fas fa-comment text-white text-2xl mr-5"></i>
+            <p class="text-white font-bold text-xl">Join the conversation.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- right side -->
+    <div class="flex w-1/2 h-full">Right</div>
   </div>
 </template>
 
@@ -9,3 +30,14 @@ export default {
   name: 'Root',
 };
 </script>
+
+<style>
+  div {
+    xborder: 1px dashed blue;
+    xpadding: 15px;
+  }
+
+  div.items-center.justify-center {
+    xbackground-color: rgba(240, 240, 240, 0.5);
+  }
+</style>
