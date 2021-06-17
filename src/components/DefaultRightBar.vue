7472db5f17b7b84e6ed6f2aa6d7fa9a5adc01b57
eduardo3g <eduardosbrasil10@gmail.com>
2021-06-17 19:42:43 -0300

    refactor: move the Trends For You section from the Ride Bar into a separate component

--- src/components/DefaultRightBar.vue
+++ src/components/DefaultRightBar.vue
@@ -2,23 +2,7 @@
    <div class="hidden md:block w-1/3 h-full border-l border-lighter px-6 py-2 overflow-y-scroll relative min-w-max">
       <SearchBar />
 
-      <div class="w-full rounded-lg bg-lightest">
-        <div class="flex items-center justify-between p-3">
-          <p class="text-lg font-bold">Trends for you</p>
-          <i class="fas fa-cog text-lg text-blue"></i>
-        </div>
-        <button v-bind:key="trend.id" v-for="trend in trending" class="w-full flex justify-between hover:bg-lighter p-3 border-t border-lighter">
-          <div>
-            <p class="text-sm text-left leading-tight text-dark">{{ trend.top }}</p>
-            <p class="font-bold text-left leading-tight">{{ trend.title }}</p>
-            <p class="text-left leading-tight text-dark">{{ trend.bottom }}</p>
-          </div>
-          <i class="fas fa-angle-down text-lg text-dark"></i>
-        </button>
-        <button class="p-3 w-full hover:bg-lighter text-left text-blue border-t border-lighter">
-          Show More
-        </button>
-      </div>
+      <TrendingForYou />
 
       <div class="w-full rounded-lg bg-lightest my-4">
         <div class="p-3">
@@ -43,21 +27,17 @@
 
 <script>
 import SearchBar from './SearchBar.vue';
+import TrendingForYou from './TrendingForYou.vue';
 
 export default {
   name: 'DefaultRightBar',
   components: {
     SearchBar,
+    TrendingForYou,
   },
   data() {
     return {
-      trending: [
-        {top: 'Trending in NL', title: 'REDALERT', bottom: '7,451 tweets', id: 'NL-READALERT'},
-        {top: 'Music', title: 'We Won', bottom: '135K Tweets', id: 'MUSIC-WE_WON'},
-        {top: 'Pop', title: 'Blue Ivy', bottom: '40k tweets', id: 'POP-BLUE_IVY'},
-        {top: 'Trending in NL', title: 'Zwarte Piet', bottom: '13.9k tweets', id: 'NL-ZWARTE_PIET'},
-        {top: 'Trending', title: 'When Beyonce', bottom: '25.4k tweets', id: 'TRENDING-WHEN_BEYONCE'},
-      ],
+      
       followSuggestions: [
         {imageUrl: 'https://i.imgur.com/jv6Hufs.jpg', name: 'Senzo', screenName: '@senzo_HQ', id: 'senzo_HQ'},
         {imageUrl: 'https://i.imgur.com/VXcv5Sj.jpg', name: 'Ant Stanley', screenName: '@IamStan', id: 'IamStan'},
