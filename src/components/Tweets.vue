62e48f7085d947e32fd8c9bf1e61bf90df516ff2
eduardo3g <eduardosbrasil10@gmail.com>
2021-07-27 14:38:32 -0300

    refactor(timeline): create separate components to isolate the Tweets list and the Tweet itself

---
+++ src/components/Tweets.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="tweet in tweets" :key="tweet.id">
+      <Tweet :tweet="tweet" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Tweet from "../components/Tweet.vue";
+
+export default {
+  name: "Tweets",
+  components: {
+    Tweet,
+  },
+  computed: {
+    ...mapGetters("twitter", {
+      nextToken: "nextTokenTweets",
+    }),
+    ...mapGetters("twitter", ["profile", "tweets"]),
+  },
+};
+</script>
