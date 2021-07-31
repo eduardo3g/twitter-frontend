b8812c8491ae1f2b82c643359397210cdd37f944
eduardo3g <eduardosbrasil10@gmail.com>
2021-07-30 21:15:12 -0300

    chore: lint the backend lib

--- src/lib/backend.js
+++ src/lib/backend.js
@@ -1,32 +1,33 @@
-import { API } from 'aws-amplify';
-import gql from 'graphql-tag';
+import { API } from "aws-amplify";
+import gql from "graphql-tag";
 
 const getMyProfile = async () => {
   const result = await API.graphql({
-      query: gql`query getMyProfile {
+    query: gql`
+      query getMyProfile {
         getMyProfile {
+          id
+          name
+          screenName
+          imageUrl
           backgroundImageUrl
           bio
+          location
+          website
           birthdate
           createdAt
           followersCount
           followingCount
-          id
-          imageUrl
-          likesCount
-          location
-          name
-          screenName
           tweetsCount
-          website
+          likesCounts
         }
-      }`,
-      authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
+      }
+    `,
+    authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
   });
-
   const profile = result.data.getMyProfile;
-  profile.imageUrl = profile.imageUrl || 'default_profile.png';
 
+  profile.imageUrl = profile.imageUrl || "default_profile.png";
   return profile;
 };
 
@@ -55,20 +56,20 @@ const getProfileByScreenName = async (screenName) => {
       }
     `,
     variables: {
-      screenName
+      screenName,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
-  const profile = result.data.getProfile
+  });
+  const profile = result.data.getProfile;
 
-  profile.imageUrl = profile.imageUrl || 'default_profile.png'
-  return profile
-}
+  profile.imageUrl = profile.imageUrl || "default_profile.png";
+  return profile;
+};
 
 const getMyTimeline = async (limit, nextToken) => {
   const result = await API.graphql({
     query: gql`
-      query getMyTimeline($limit:Int!, $nextToken:String) {
+      query getMyTimeline($limit: Int!, $nextToken: String) {
         getMyTimeline(limit: $limit, nextToken: $nextToken) {
           nextToken
           tweets {
@@ -168,22 +169,22 @@ const getMyTimeline = async (limit, nextToken) => {
     `,
     variables: {
       limit,
-      nextToken
+      nextToken,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
+  });
 
-  return result.data.getMyTimeline
-}
+  return result.data.getMyTimeline;
+};
 
 const getTweets = async (userId, limit, nextToken) => {
   const result = await API.graphql({
     query: gql`
-      query getTweets($userId:ID!, $limit:Int!, $nextToken:String) {
-        getTweets(userId:$userId, limit:$limit, nextToken: $nextToken) {
+      query getTweets($userId: ID!, $limit: Int!, $nextToken: String) {
+        getTweets(userId: $userId, limit: $limit, nextToken: $nextToken) {
           nextToken
-          tweets {           
-            __typename 
+          tweets {
+            __typename
             id
             profile {
               id
@@ -275,35 +276,35 @@ const getTweets = async (userId, limit, nextToken) => {
     variables: {
       userId,
       limit,
-      nextToken
+      nextToken,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
+  });
 
-  return result.data.getTweets
-}
+  return result.data.getTweets;
+};
 
 const getImageUploadUrl = async (extension, contentType) => {
   const result = await API.graphql({
     query: gql`
-      query getImageUploadUrl($extension:String, $contentType:String) {
-        getImageUploadUrl(extension:$extension, contentType:$contentType) 
+      query getImageUploadUrl($extension: String, $contentType: String) {
+        getImageUploadUrl(extension: $extension, contentType: $contentType)
       }
     `,
     variables: {
       extension,
-      contentType
+      contentType,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
+  });
 
-  return result.data.getImageUploadUrl
-}
+  return result.data.getImageUploadUrl;
+};
 
 const editMyProfile = async (newProfile) => {
   const result = await API.graphql({
     query: gql`
-      mutation editMyProfile($newProfile:ProfileInput!) {
+      mutation editMyProfile($newProfile: ProfileInput!) {
         editMyProfile(newProfile: $newProfile) {
           backgroundImageUrl
           bio
@@ -323,17 +324,17 @@ const editMyProfile = async (newProfile) => {
       }
     `,
     variables: {
-      newProfile
+      newProfile,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
-  return result.data.editMyProfile
-}
+  });
+  return result.data.editMyProfile;
+};
 
 const tweet = async (text) => {
   const result = await API.graphql({
     query: gql`
-      mutation tweet($text:String!) {
+      mutation tweet($text: String!) {
         tweet(text: $text) {
           createdAt
           id
@@ -352,17 +353,17 @@ const tweet = async (text) => {
       }
     `,
     variables: {
-      text
+      text,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
-  return result.data.tweet
-}
+  });
+  return result.data.tweet;
+};
 
 const retweet = async (tweetId) => {
   await API.graphql({
     query: gql`
-      mutation retweet($tweetId:ID!) {
+      mutation retweet($tweetId: ID!) {
         retweet(tweetId: $tweetId) {
           id
           createdAt
@@ -370,58 +371,58 @@ const retweet = async (tweetId) => {
       }
     `,
     variables: {
-      tweetId
+      tweetId,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
-}
+  });
+};
 
 const unretweet = async (tweetId) => {
   await API.graphql({
     query: gql`
-      mutation unretweet($tweetId:ID!) {
+      mutation unretweet($tweetId: ID!) {
         unretweet(tweetId: $tweetId)
       }
     `,
     variables: {
-      tweetId
+      tweetId,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
-}
+  });
+};
 
 const like = async (tweetId) => {
   await API.graphql({
     query: gql`
-      mutation like($tweetId:ID!) {
+      mutation like($tweetId: ID!) {
         like(tweetId: $tweetId)
       }
     `,
     variables: {
-      tweetId
+      tweetId,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
-}
+  });
+};
 
 const unlike = async (tweetId) => {
   await API.graphql({
     query: gql`
-      mutation unlike($tweetId:ID!) {
+      mutation unlike($tweetId: ID!) {
         unlike(tweetId: $tweetId)
       }
     `,
     variables: {
-      tweetId
+      tweetId,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
-}
+  });
+};
 
 const reply = async (tweetId, text) => {
   const result = await API.graphql({
     query: gql`
-      mutation reply($tweetId:ID!, $text:String!) {
+      mutation reply($tweetId: ID!, $text: String!) {
         reply(tweetId: $tweetId, text: $text) {
           id
           createdAt
@@ -441,46 +442,46 @@ const reply = async (tweetId, text) => {
     `,
     variables: {
       tweetId,
-      text
+      text,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
+  });
 
-  return result.data.reply
-}
+  return result.data.reply;
+};
 
 const follow = async (userId) => {
   await API.graphql({
     query: gql`
-      mutation follow($userId:ID!) {
+      mutation follow($userId: ID!) {
         follow(userId: $userId)
       }
     `,
     variables: {
-      userId
+      userId,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
-}
+  });
+};
 
 const unfollow = async (userId) => {
   await API.graphql({
     query: gql`
-      mutation unfollow($userId:ID!) {
+      mutation unfollow($userId: ID!) {
         unfollow(userId: $userId)
       }
     `,
     variables: {
-      userId
+      userId,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
-}
+  });
+};
 
 const getFollowers = async (userId, limit = 10) => {
   const result = await API.graphql({
     query: gql`
-      query getFollowers($userId:ID!, $limit:Int!) {
+      query getFollowers($userId: ID!, $limit: Int!) {
         getFollowers(userId: $userId, limit: $limit) {
           profiles {
             id
@@ -492,25 +493,25 @@ const getFollowers = async (userId, limit = 10) => {
               following
               followedBy
             }
-          },
+          }
           nextToken
         }
       }
     `,
     variables: {
       userId,
-      limit
+      limit,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
+  });
 
-  return result.data.getFollowers
-}
+  return result.data.getFollowers;
+};
 
 const getFollowing = async (userId, limit = 10) => {
   const result = await API.graphql({
     query: gql`
-      query getFollowing($userId:ID!, $limit:Int!) {
+      query getFollowing($userId: ID!, $limit: Int!) {
         getFollowing(userId: $userId, limit: $limit) {
           profiles {
             id
@@ -522,20 +523,20 @@ const getFollowing = async (userId, limit = 10) => {
               following
               followedBy
             }
-          },
+          }
           nextToken
         }
       }
     `,
     variables: {
       userId,
-      limit
+      limit,
     },
     authMode: process.env.VUE_APP_APPSYNC_AUTHENTICATION_TYPE,
-  })
+  });
 
-  return result.data.getFollowing
-}
+  return result.data.getFollowing;
+};
 
 export {
   getMyProfile,
