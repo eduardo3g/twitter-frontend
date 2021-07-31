26be10ad041e43abab500b38c4fc95152ec0418d
eduardo3g <eduardosbrasil10@gmail.com>
2021-07-30 21:48:32 -0300

    fix(backend): rename the field from likesCounts to likesCount

--- src/lib/backend.js
+++ src/lib/backend.js
@@ -19,7 +19,7 @@ const getMyProfile = async () => {
           followersCount
           followingCount
           tweetsCount
-          likesCounts
+          likesCount
         }
       }
     `,
@@ -49,7 +49,7 @@ const getProfileByScreenName = async (screenName) => {
           followersCount
           followingCount
           tweetsCount
-          likesCounts
+          likesCount
           following
           followedBy
         }
@@ -314,7 +314,7 @@ const editMyProfile = async (newProfile) => {
           followingCount
           id
           imageUrl
-          likesCounts
+          likesCount
           location
           name
           screenName
