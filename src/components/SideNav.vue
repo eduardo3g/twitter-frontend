225a0a343ef1e41140a631d62aa54f4ef4d19b4f
eduardo3g <eduardosbrasil10@gmail.com>
2021-08-05 18:10:44 -0300

    style: create the Profile view

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,89 +1,143 @@
 <template>
-  <div class="lg:w-1/5 border-r border-lighter px-2 lg:px-6 py-2 flex flex-col justify-between min-w-max">
+  <div
+    class="lg:w-1/5 border-r border-lighter px-2 lg:px-6 py-2 flex flex-col justify-between min-w-max"
+  >
     <div>
-      <button class="h-12 w-12 hover:bg-lightblue text-3xl text-blue rounded-full">
+      <button
+        class="h-12 w-12 hover:bg-lightblue text-3xl text-blue rounded-full"
+      >
         <i class="fab fa-twitter"></i>
       </button>
       <div>
-        <button 
-          v-for="tab in tabs" :key="tab.id"
-          class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-lightblue rounded-full mr-auto mb-3">
+        <button
+          v-for="tab in tabs"
+          :key="tab.id"
+          @click="open(tab.target)"
+          class="focus:outline-none hover:text-blue flex items-center px-4 py-2 hover:bg-lightblue rounded-full mr-auto mb-3"
+        >
           <i class="text-2xl mr-4" :class="tab.icon"></i>
-          <p class="text-lg font-semibold text-left hidden lg:block"> {{ tab.title }}</p>
+          <p class="text-lg font-semibold text-left hidden lg:block">
+            {{ tab.title }}
+          </p>
         </button>
       </div>
-      <button class="text-white bg-blue rounded-full font-semibold h-12 lg:h-auto w-20 lg:w-full p-3 hover:bg-darkblue">
+      <button
+        class="text-white bg-blue rounded-full font-semibold h-12 lg:h-auto w-20 lg:w-full p-3 hover:bg-darkblue"
+      >
         <p class="hidden lg:block">Tweet</p>
         <i class="fas fa-plus lg:hidden"></i>
       </button>
     </div>
 
     <div class="lg:w-full relative">
-      <button @click="dropdown = !dropdown" class="flex items-center w-full hover:bg-lightblue rounded-full p-2">
-        <img :src="`${profile.imageUrl || 'default_profile.png'}`" class="w-10 h-10 rounded-full" />
+      <button
+        @click="dropdown = !dropdown"
+        class="flex items-center w-full hover:bg-lightblue rounded-full p-2"
+      >
+        <img
+          :src="`${profile.imageUrl || 'default_profile.png'}`"
+          class="w-10 h-10 rounded-full"
+        />
         <div class="hidden lg:block ml-4 truncate">
-          <div class="text-left text-sm font-bold leading-tight truncate">{{ profile.name }}</div>
-          <div class="text-left text-sm leading-tight text-dark truncate">{{ profile.screenName }}</div>
+          <div class="text-left text-sm font-bold leading-tight truncate">
+            {{ profile.name }}
+          </div>
+          <div class="text-left text-sm leading-tight text-dark truncate">
+            {{ profile.screenName }}
+          </div>
         </div>
         <i class="hidden lg:block fas fa-angle-down ml-auto text-lg"></i>
       </button>
-      <div v-if="dropdown === true" class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-lightest bg-white mb-16">
-        <button @click="dropdown = false" class="p-3 flex items-center w-full hover:bg-lightest">
-          <img :src="`${profile.imageUrl || 'default_profile.png'}`" class="w-10 h-10 rounded-full" />
+      <div
+        v-if="dropdown === true"
+        class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-lightest bg-white mb-16"
+      >
+        <button
+          @click="dropdown = false"
+          class="p-3 flex items-center w-full hover:bg-lightest"
+        >
+          <img
+            :src="`${profile.imageUrl || 'default_profile.png'}`"
+            class="w-10 h-10 rounded-full"
+          />
           <div class="ml-4">
-            <p class="text-left text-sm font-bold leading-tight">{{ profile.name }}</p>
-            <p class="text-left text-sm leading-tight text-dark">{{ profile.screenName }}</p>
+            <p class="text-left text-sm font-bold leading-tight">
+              {{ profile.name }}
+            </p>
+            <p class="text-left text-sm leading-tight text-dark">
+              {{ profile.screenName }}
+            </p>
           </div>
           <i class="fas fa-check ml-auto text-blue"></i>
         </button>
-        <button class="w-full text-left hover:bg-lightest border-t border-lighter p-3 text-sm">
+        <button
+          class="w-full text-left hover:bg-lightest border-t border-lighter p-3 text-sm"
+        >
           Add an existing account
         </button>
-        <button @click="signOut" class="w-full text-left hover:bg-lightest border-t border-lighter p-3 text-sm">
+        <button
+          @click="signOut"
+          class="w-full text-left hover:bg-lightest border-t border-lighter p-3 text-sm"
+        >
           Log out {{ profile.screenName }}
         </button>
       </div>
-    </div>    
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from "vuex";
 
 export default {
-  name: 'SideNav',
+  name: "SideNav",
   data() {
     return {
       tabs: [
-        {icon: 'fas fa-home', title: 'Home', id: 'home', target: 'Home'},
-        {icon: 'fas fa-hashtag', title: 'Explore', id: 'explore'},
-        {icon: 'far fa-bell', title: 'Notifications', id: 'notifications'},
-        {icon: 'far fa-envelope', title: 'Messages', id: 'messages'},
-        {icon: 'far fa-bookmark', title: 'Bookmarks', id: 'bookmarks'},
-        {icon: 'fas fa-clipboard-list', title: 'Lists', id: 'lists'},
-        {icon: 'far fa-user', title: 'Profile', id: 'profile', target: 'Profile'},
-        {icon: 'fas fa-ellipsis-h', title: 'More', id: 'more'}
+        { icon: "fas fa-home", title: "Home", id: "home", target: "Home" },
+        { icon: "fas fa-hashtag", title: "Explore", id: "explore" },
+        { icon: "far fa-bell", title: "Notifications", id: "notifications" },
+        { icon: "far fa-envelope", title: "Messages", id: "messages" },
+        { icon: "far fa-bookmark", title: "Bookmarks", id: "bookmarks" },
+        { icon: "fas fa-clipboard-list", title: "Lists", id: "lists" },
+        {
+          icon: "far fa-user",
+          title: "Profile",
+          id: "profile",
+          target: "Profile",
+        },
+        { icon: "fas fa-ellipsis-h", title: "More", id: "more" },
       ],
       dropdown: false,
     };
   },
   computed: {
-    ...mapGetters('twitter', [
-      'profile',
-    ]),
+    ...mapGetters("twitter", ["profile"]),
   },
   methods: {
-    ...mapActions('authentication', [
-      'logoutUser',
-    ]),
+    ...mapActions("authentication", ["logoutUser"]),
+    async open(target) {
+      const current = this.$route.name;
+      if (
+        target != current ||
+        this.$route.params.screenName != this.profile.screenName
+      ) {
+        this.$router.push({
+          name: target,
+          params: {
+            screenName: this.profile.screenName,
+          },
+        });
+      }
+    },
     async signOut() {
       try {
-        console.log('signing out...');
+        console.log("signing out...");
         await this.logoutUser();
       } catch (e) {
         alert(`Error signing out: ${e.message}`);
       }
-    }
+    },
   },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,45 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Root from '../views/Root.vue';
-import AuthMiddleware from './auth.guard';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Root from "../views/Root.vue";
+import AuthMiddleware from "./auth.guard";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Root',
+    path: "/",
+    name: "Root",
     component: Root,
   },
   {
-    path: '/login',
-    name: 'Login',
+    path: "/login",
+    name: "Login",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "login" */ '../views/LogIn.vue')
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/LogIn.vue"),
   },
   {
-    path: '/home',
-    name: 'Home',
+    path: "/home",
+    name: "Home",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
     meta: { protected: true },
-  }
-]
+  },
+  {
+    path: "/:screenName",
+    name: "Profile",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "home" */ "../views/Profile.vue"),
+    meta: { protected: true },
+  },
+];
 
 const router = new VueRouter({
   routes,
