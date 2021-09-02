a8aa9cb9643df6a1aba1c8f98cfacfb8e43f7212
eduardo3g <eduardosbrasil10@gmail.com>
2021-09-02 18:51:39 -0300

    style: create Users component to display a list of 'User' components

---
+++ src/components/Users.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span v-if="users.length === 0">No results</span>
+    <div v-bind:key="user.id" v-for="user in users">
+      <User :user="user" />
+    </div>
+  </div>
+</template>
+
+<script>
+import User from "./User.vue";
+
+export default {
+  name: "Users",
+  props: ["users"],
+  components: {
+    User,
+  },
+};
+</script>
